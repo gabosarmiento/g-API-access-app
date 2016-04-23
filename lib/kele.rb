cb40c52309909dcee9dfd628c8cb0a0f8deab5c3
gabosarmiento <gabrielsarmiento@gmail.com>
2016-04-23 00:53:27 -0500

    retrieve users

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+# test this code in irb after typing => $: << 'lib'
 class Kele
   include HTTParty
   attr_accessor :auth_token
@@ -7,5 +8,10 @@ class Kele
   def initialize(email, password)
     @auth_token = self.class.post('/sessions', body: { email: email, password: password })["auth_token"]
     raise StandardError.new('Invalid credentials') unless @auth_token
+  end
+
+  def get_me
+    response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
   end
 end
